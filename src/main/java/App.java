d1f7fd3f50ee6182552963350fffbbb220ca8f28
Дмитрий <d.xoxrin2010@yandex.ru>
2023-02-23 14:37:57 +0300

    fix

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(bean1));
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.equals(beanCat2));
+
         System.out.println(bean.getMessage());
     }
 }
